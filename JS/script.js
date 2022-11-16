4e87340601cc3720a0cec2769c02bcee36351ecc
lourdesquintana <lulyarismendi@gmail.com>
2022-11-15 23:59:32 -0300

    suma num 1+ num 2

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,11 @@ console.log(nombre);
 console.log(nombre2);
 console.log(dni);
 //#endregion nombres
+
+//#region suma
+let num1 = 60;
+let num2 = 60;
+function suma(num1, num2) {
+    let k = num1 + num2;
+    return k;
+}
