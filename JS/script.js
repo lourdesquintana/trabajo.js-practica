26955f147d27d7b1db3325d685d0a965a3e41e3b
lourdesquintana <lulyarismendi@gmail.com>
2022-11-15 23:58:35 -0300

    console.log con las variables

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,9 @@ const nombre = `luli`;
 let nombre2 = "lourdes";
 var dni = '46330054';
 //#endregion variables
+
+//#region nombres
+console.log(nombre);
+console.log(nombre2);
+console.log(dni);
+//#endregion nombres
